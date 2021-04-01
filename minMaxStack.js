9a78fc6fb35668e1cf42cc92e3782d442a195d43
Prajwal <61734018+prajwalr308@users.noreply.github.com>
2021-04-01 21:32:14 +0530

    minMaxstack

---
+++ minMaxStack.js
@@ -0,0 +1,35 @@
+class MinMaxStack {
+  constructor() {
+    (this.minMaxStack = []), (this.stack = []);
+  }
+  peek() {
+    return this.stack[this.stack.length - 1];
+  }
+
+  pop() {
+    this.minMaxStack.pop();
+    return this.stack.pop();
+  }
+
+  push(number) {
+    let newMinMax = { min: number, max: number };
+    if (this.minMaxStack.length) {
+      let lastMinMax = this.minMaxStack[this.minMaxStack.length - 1];
+      newMinMax.min = Math.min(lastMinMax.min, number);
+      newMinMax.max = Math.max(lastMinMax.max, number);
+    }
+    this.minMaxStack.push(newMinMax);
+    this.stack.push(number);
+  }
+
+  getMin() {
+    return this.minMaxStack[this.minMaxStack.length - 1].min;
+  }
+
+  getMax() {
+    return this.minMaxStack[this.minMaxStack.length - 1].max;
+  }
+}
+
+// Do not edit the line below.
+exports.MinMaxStack = MinMaxStack;
