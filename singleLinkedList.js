b5a8f4ac76807bda4221f13ba7db5005b7fadbbd
prajwalr308 <prajwalr.thedeveloper@gmail.com>
2021-05-02 18:11:47 +0530

    single linked list

---
+++ singleLinkedList.js
@@ -0,0 +1,49 @@
+class Node{
+    constructor(value){
+        this.value=value;
+        this.next=null;
+    }
+}
+class List{
+    constructor(){
+        this.head=new Node("head");
+       
+        
+
+    }
+    find(item){
+        var currNode=this.head;
+        while(currNode.value!=item){
+            currNode=currNode.next;
+        }
+        console.log("find",currNode)
+        return currNode;
+        
+    }
+    insert(value,item){
+        var newNode= new Node(value);
+  
+        var current=this.find(item);
+ 
+        newNode.next=current.next;
+      
+        current.next=newNode;
+      
+
+    }
+    display(){
+        var currNode=this.head;
+        while(currNode){
+            currNode=currNode.next;
+            console.log(currNode)
+        }
+      
+    }
+
+}
+const test =new List();
+test.insert(2,"head");
+test.insert(1,2);
+test.insert(3,1);
+test.display()
+
