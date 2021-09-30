5c7e674bb98f03bd4525e12a08445dcc11fa511d
Prajwal <61734018+prajwalr308@users.noreply.github.com>
2021-09-30 19:01:06 +0530

    corrections

--- fibonacci.js
+++ fibonacci.js
@@ -1,8 +1,8 @@
 function getNthFib(n) {
     // Write your code here.
-      if(n==2){
+      if(n==1){
           return 1;
-      }else if(n==1){
+      }else if(n==0){
           return 0
       }else{
           return getNthFib(n-1)+getNthFib(n-2)
